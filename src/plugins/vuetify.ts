9f32533846e323253a27ea8fab012d840fb9c05c
Pinkie Aruwani <aruwanip@gmail.com>
2019-10-06 17:20:22 -0400

    Install Vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,4 +7,17 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
+  theme: {
+    themes: {
+      light: {
+        primary: '#8bc34a',
+        secondary: '#009688',
+        accent: '#e91e63',
+        error: '#f44336',
+        warning: '#ffc107',
+        info: '#03a9f4',
+        success: '#4caf50'
+      }
+    }
+  }
 });
